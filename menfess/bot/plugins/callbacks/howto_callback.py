c2d0d54e96efe38409b5babb202e9dd93ea6b0ae
Muhammad Rizki <kiizuha@gnuweeb.org>
2022-12-04 18:06:34 +0700

    v1.0.0-rc.3: add a howto callback
    To make a button that contain message to tell user how to use this bot.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

---
+++ menfess/bot/plugins/callbacks/howto_callback.py
@@ -0,0 +1,20 @@
+from pyrogram import Client
+from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
+
+from . import filter
+from menfess.bot.templates import ON_HOWTO_CB
+
+
+@Client.on_callback_query(filter.callback("howto"))
+async def on_howto_callback(_, cq: CallbackQuery):
+	user_id = cq.from_user.id
+	msg = ON_HOWTO_CB.format(
+		user_id=user_id,
+		channel_link="t.me/teyvat_realm"
+	)
+	await cq.edit_message_text(msg, reply_markup=InlineKeyboardMarkup(
+		[[InlineKeyboardButton(
+			"◀️ Kembali",
+			callback_data=f"start {user_id}"
+		)]]
+	))
