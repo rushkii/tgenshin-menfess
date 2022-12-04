9a267e59c90ef8b16def419878d192a93e60bf57
Muhammad Rizki <kiizuha@gnuweeb.org>
2022-12-04 19:10:41 +0700

    v1.0.0-rc.4: add CHANNEL_USERNAME env var
    Add CHANNEL_USERNAME environment variable to store the Menfess Telegram
    channel username to create a channel URL.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- menfess/__init__.py
+++ menfess/__init__.py
@@ -2,6 +2,6 @@ __author__ = "Muhammad Rizki"
 __author_nickname__ = "Kiizuha"
 __author_email__ = "kiizuha@gnuweeb.org"
 
-__version__ = "1.0.0-rc.3"
+__version__ = "1.0.0-rc.4"
 __license__ = "GNU Lesser General Public License v3.0"
 __copyright__ = "Copyright (C) 2022-present Kiizuha <https://github.com/kiizuha>"
