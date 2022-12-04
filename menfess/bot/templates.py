8cd1639cdb44b66c61e9833abf39a80d6385711b
Muhammad Rizki <kiizuha@gnuweeb.org>
2022-12-04 18:10:54 +0700

    v1.0.0-rc3: add a bot message template
    Add a message template to easily send a message to a user, the template
    has been formatted, so just call them and use .format() kwargs.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

---
+++ menfess/bot/templates.py
@@ -0,0 +1,20 @@
+__TRAVELER = "[Traveler](tg://user?id={user_id})"
+__TEYVAT = "[Teyvat]({channel_link})"
+
+
+ON_START_MSG = """
+Halo {_TRV}!, selamat datang di {bot_name}.
+Aku adalah bot untuk membawa pesan kamu ke alam {_TVT},
+{_TRV} kalau mau tau cara kirim pesan ke {_TVT} tinggal tekan tombol di bawah ya, ehe!
+""".format(_TRV=__TRAVELER, _TVT=__TEYVAT, bot_name="{bot_name}")
+
+
+ON_HOWTO_CB = """
+{_TRV} tinggal ketik apapun yang {_TRV} mau yang berhubungan dengan Genshin Impact ya {_TRV}, tetapi
+pesan {_TRV} akan terkirim jika pesan {_TRV} memenuhi syarat di bawah ini:
+- Akun Telegram {_TRV} harus follow {_TVT} dulu ya, ehe~.
+- Akun Telegram {_TRV} harus memiliki username dan foto profil.
+- Pesan {_TRV} harus berjumlah minimal 20 karakter dan 5 kata.
+
+Udah itu aja {_TRV}! enjoy flexing gacha dan damage karakter {_TRV} ya, ehe!
+""".format(_TRV=__TRAVELER, _TVT=__TEYVAT)
