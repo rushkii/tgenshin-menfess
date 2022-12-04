6c82ce48dd33c7b4a3b54f94229ae9d933ae2237
Muhammad Rizki <kiizuha@gnuweeb.org>
2022-12-04 22:36:02 +0700

    v1.0.0-rc.5: add a menfess post handler
    This handler is to handle a user who want to post their message into the
    menfess channel, which is user message and user acccount must met the
    required rules.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

---
+++ menfess/bot/plugins/post_menfess.py
@@ -0,0 +1,28 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from menfess.bot.templates import ON_SUCCESS_POST
+
+
+@Client.on_message(
+	(filters.text | filters.photo | filters.video | filters.document) &
+	filters.private
+)
+async def on_post_menfess(c: Client, m: Message):
+	if len(m.text.split()) >= 5 and len(m.text) >= 20:
+		ch_usn = os.getenv("CHANNEL_USERNAME")
+		mem_ids = [mem.user.id async for mem in c.get_chat_members(ch_usn)]
+		if m.from_user.id in mem_ids:
+			copied = await m.copy(ch_usn)
+			msg = ON_SUCCESS_POST.format(user_id=m.from_user.id)
+			await m.reply(
+				text=msg,
+				reply_markup=InlineKeyboardMarkup(
+					[[InlineKeyboardButton(
+						"Lihat pesan 💬",
+						url=f"t.me/{ch_usn}/{copied.id}"
+					)]]
+				),
+				disable_web_page_preview=True
+			)
