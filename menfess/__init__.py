45cba91b1368baa0ea14411ff2d90c988a5281b9
Muhammad Rizki <kiizuha@gnuweeb.org>
2022-12-04 17:56:37 +0700

    v1.0.0-rc.3: add __init__.py in the core package directory.
    Add some author information and software information.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

---
+++ menfess/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Muhammad Rizki"
+__author_nickname__ = "Kiizuha"
+__author_email__ = "kiizuha@gnuweeb.org"
+
+__version__ = "1.0.0-rc.3"
+__license__ = "GNU Lesser General Public License v3.0"
+__copyright__ = "Copyright (C) 2022-present Kiizuha <https://github.com/kiizuha>"
