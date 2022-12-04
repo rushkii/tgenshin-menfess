db3418cb64b33baf55356055697a604e549e0753
Muhammad Rizki <kiizuha@gnuweeb.org>
2022-12-04 19:20:36 +0700

    v1.0.0-rc.5: add load_dotenv() in the menfess/__init__.py
    Add load_dotenv() to load all environment variables in the .env file.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- menfess/__init__.py
+++ menfess/__init__.py
@@ -1,7 +1,13 @@
+from dotenv import load_dotenv
+
+
 __author__ = "Muhammad Rizki"
 __author_nickname__ = "Kiizuha"
 __author_email__ = "kiizuha@gnuweeb.org"
 
-__version__ = "1.0.0-rc.4"
+__version__ = "1.0.0-rc.5"
 __license__ = "GNU Lesser General Public License v3.0"
 __copyright__ = "Copyright (C) 2022-present Kiizuha <https://github.com/kiizuha>"
+
+
+load_dotenv()
