6f1c3208b8d800a44bb4471f2d5170fa995fcccf
Muhammad Rizki <kiizuha@gnuweeb.org>
2022-12-09 00:42:47 +0700

    env fix: add DB_NAME environment variable
    Forgot to add a DB_NAME environment variable in the .env-example.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- menfess/__init__.py
+++ menfess/__init__.py
@@ -8,7 +8,7 @@ __author__ = "Muhammad Rizki"
 __author_nickname__ = "Kiizuha"
 __author_email__ = "kiizuha@gnuweeb.org"
 
-__version__ = "1.1.0-rc2"
+__version__ = "1.1.1-rc1"
 __license__ = "GNU Lesser General Public License v3.0"
 __copyright__ = "Copyright (C) 2022-present Kiizuha <https://github.com/kiizuha>"
 
