8d1d2c0e6c320604faeb490b3417f738237b95f0
Muhammad Rizki <kiizuha@gnuweeb.org>
2022-12-11 07:09:07 +0700

    bot: templates: add ON_FAILED_POST string template
    Add ON_FAILED_POST for sending a message if a user met the requirements
    to send a message to the channel.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- menfess/bot/templates.py
+++ menfess/bot/templates.py
@@ -3,6 +3,9 @@ import os
 
 __TRAVELER = "[Traveler](tg://user?id={user_id})"
 __TEYVAT = f"[Teyvat](t.me/{os.getenv('CHANNEL_USERNAME')})"
+__RULES = """- Akun Telegram {_TRV} harus follow {_TVT} dulu ya, ehe~.
+- Akun Telegram {_TRV} harus memiliki username dan foto profil.
+- Pesan {_TRV} harus berjumlah minimal 20 karakter dan 5 kata.""".format(_TRV=__TRAVELER, _TVT=__TEYVAT)
 
 
 ON_START_MSG = """
@@ -15,15 +18,23 @@ Aku adalah bot untuk membawa pesan kamu ke alam {_TVT},
 ON_HOWTO_CB = """
 {_TRV} tinggal ketik apapun yang {_TRV} mau yang berhubungan dengan Genshin Impact ya {_TRV}, tetapi
 pesan {_TRV} akan terkirim jika pesan {_TRV} memenuhi syarat di bawah ini:
-- Akun Telegram {_TRV} harus follow {_TVT} dulu ya, ehe~.
-- Akun Telegram {_TRV} harus memiliki username dan foto profil.
-- Pesan {_TRV} harus berjumlah minimal 20 karakter dan 5 kata.
+
+{_RULE}
 
 Udah itu aja {_TRV}! enjoy flexing gacha dan damage karakter {_TRV} ya, ehe!
-""".format(_TRV=__TRAVELER, _TVT=__TEYVAT)
+""".format(_TRV=__TRAVELER, _TVT=__TEYVAT, _RULE=__RULES)
 
 
 ON_SUCCESS_POST = """
 Hore, pesan {_TRV} telah terkirim ke {_TVT}!
 {_TRV} bisa melihatnya dengan menekan tombol di bawah ya~.
 """.format(_TRV=__TRAVELER, _TVT=__TEYVAT)
+
+
+ON_FAILED_POST = """
+Yah, pesan {_TRV} belum memenuhi kriteria di bawah ini:
+
+{_RULE}
+
+Jadi Paimon gabisa mengirim pesan {_TRV}, hiks.
+""".format(_TRV=__TRAVELER, _TVT=__TEYVAT, _RULE=__RULES)
