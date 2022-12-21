250d2782b13ef06374ef41f2c2737d507b059e5f
Muhammad Rizki <kiizuha@gnuweeb.org>
2022-12-22 04:33:04 +0700

    copyright: add GNU General Public License v3.0 notice in every files
    We add a GPL notice to remind users not to sell or resell of our
    programs, our programs is only for educational purpose only and personal
    use only.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- menfess/bot/plugins/commands/start.py
+++ menfess/bot/plugins/commands/start.py
@@ -1,3 +1,29 @@
+#  TGenshin Menfess - A Telegram bot for Genshin Impact "Mention and Confess"
+#  Copyright (C) 2022-present Kiizuha <https://github.com/rushkii>
+#
+#  This file is part of tgenshin-menfess.
+#
+#  tgenshin-menfess is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  tgenshin-menfess is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with tgenshin-menfess. If not, see <https://www.gnu.org/licenses/>.
+#
+#  DO NO SELL OR RESELL THIS PROGRAM, THIS PROGRAM IS FOR EDUCATIONAL PURPOSE ONLY
+#  AND PERSONAL USE ONLY. YOU CAN USE THIS PROGRAM AND MONETIZE IT LIKE A DONATION
+#  FOR YOUR BOT AND YOUR SERVER HOSTING USING MY SCRIPTS. BUT, YOU SHOULD PUT
+#  MY AUTHOR NAME IN YOUR BOT DESCRIPTION, ABOUT, ETC. YOU SHOULD PUT MY EMAIL
+#  <kiizuha@gnuweeb.org> AND MY GITHUB <https://github.com/rushkii> IN YOUR BOT DESC
+#  AND REMAIN KEEP OF MY LICENSE NOTICE.
+
+
 from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
